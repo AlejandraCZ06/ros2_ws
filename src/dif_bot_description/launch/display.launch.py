795007af14fce44fa7e2e8bff31f78cdfcb293ad
AlejandraCZ06 <alejandra.lea.2023@gmail.com>
2026-09-15 21:04:05 -0500

    cambios

---
+++ src/dif_bot_description/launch/display.launch.py
@@ -0,0 +1,27 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+import xacro
+
+def generate_launch_description():
+    pkg_path = get_package_share_directory('dif_bot_description')
+    xacro_file = os.path.join(pkg_path, 'urdf', 'dif_bot.urdf.xacro')
+    robot_description_config = xacro.process_file(xacro_file)
+
+    return LaunchDescription([
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            parameters=[{'robot_description': robot_description_config.toxml()}]
+        ),
+        Node(
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui'
+        ),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            output='screen'
+        )
+    ])
